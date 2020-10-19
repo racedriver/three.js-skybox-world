79eb38cb864cb85f415c04c3c7525f6c0fe4673b
Alex Mittler <alex@skylines.one>
2020-10-20 01:38:42 +0200

    Initial commit

--- main.js
+++ main.js
@@ -4,136 +4,187 @@ import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/j
 
 
 class BasicWorldDemo {
-  constructor() {
-    this._Initialize();
-  }
 
-  _Initialize() {
-    this._threejs = new THREE.WebGLRenderer({
-      antialias: true,
-    });
-    this._threejs.shadowMap.enabled = true;
-    this._threejs.shadowMap.type = THREE.PCFSoftShadowMap;
-    this._threejs.setPixelRatio(window.devicePixelRatio);
-    this._threejs.setSize(window.innerWidth, window.innerHeight);
+    constructor() {
+        this._Initialize();
+    }
 
-    document.body.appendChild(this._threejs.domElement);
+    _Initialize() {
+        this._threejs = new THREE.WebGLRenderer({
+            antialias: true,
+        });
+        this._threejs.shadowMap.enabled = true;
+        this._threejs.shadowMap.type = THREE.PCFSoftShadowMap;
+        this._threejs.setPixelRatio(window.devicePixelRatio);
+        this._threejs.setSize(window.innerWidth, window.innerHeight);
 
-    window.addEventListener('resize', () => {
-      this._OnWindowResize();
-    }, false);
+        document.body.appendChild(this._threejs.domElement);
 
-    const fov = 60;
-    const aspect = 1920 / 1080;
-    const near = 1.0;
-    const far = 1000.0;
-    this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    this._camera.position.set(75, 20, 0);
+        window.addEventListener('resize', () => {
+            this._OnWindowResize();
+        }, false);
 
-    this._scene = new THREE.Scene();
+        const fov = 55;
+        const aspect = 1920 / 1080;
+        const near = 45;
+        const far = 30000;
+        this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+        this._camera.position.set(-900, -200, 900);
 
-    let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-    light.position.set(20, 100, 10);
-    light.target.position.set(0, 0, 0);
-    light.castShadow = true;
-    light.shadow.bias = -0.001;
-    light.shadow.mapSize.width = 2048;
-    light.shadow.mapSize.height = 2048;
-    light.shadow.camera.near = 0.1;
-    light.shadow.camera.far = 500.0;
-    light.shadow.camera.near = 0.5;
-    light.shadow.camera.far = 500.0;
-    light.shadow.camera.left = 100;
-    light.shadow.camera.right = -100;
-    light.shadow.camera.top = 100;
-    light.shadow.camera.bottom = -100;
-    this._scene.add(light);
+        this._scene = new THREE.Scene();
 
-    light = new THREE.AmbientLight(0x101010);
-    this._scene.add(light);
+        let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+        light.position.set(20, 100, 10);
+        light.target.position.set(0, 0, 0);
+        light.castShadow = true;
+        light.shadow.bias = -0.001;
+        light.shadow.mapSize.width = 2048;
+        light.shadow.mapSize.height = 2048;
+        light.shadow.camera.near = 0.1;
+        light.shadow.camera.far = 500.0;
+        light.shadow.camera.near = 0.5;
+        light.shadow.camera.far = 500.0;
+        light.shadow.camera.left = 100;
+        light.shadow.camera.right = -100;
+        light.shadow.camera.top = 100;
+        light.shadow.camera.bottom = -100;
+        this._scene.add(light);
 
-    const controls = new OrbitControls(
-      this._camera, this._threejs.domElement);
-    controls.target.set(0, 20, 0);
-    controls.update();
+        light = new THREE.AmbientLight(0x101010);
+        this._scene.add(light);
 
-    const loader = new THREE.CubeTextureLoader();
-    const texture = loader.load([
-        './resources/posx.jpg',
-        './resources/negx.jpg',
-        './resources/posy.jpg',
-        './resources/negy.jpg',
-        './resources/posz.jpg',
-        './resources/negz.jpg',
-    ]);
-    this._scene.background = texture;
+        //const controls = new OrbitControls(
+        //    this._camera, this._threejs.domElement);
+        //controls.target.set(0, 20, 0);
+        //controls.update();
 
-    const plane = new THREE.Mesh(
-        new THREE.PlaneGeometry(100, 100, 10, 10),
-        new THREE.MeshStandardMaterial({
-            color: 0xFFFFFF,
-          }));
-    plane.castShadow = false;
-    plane.receiveShadow = true;
-    plane.rotation.x = -Math.PI / 2;
-    this._scene.add(plane);
+      let controls = new OrbitControls(this._camera, this._threejs.domElement);
+      controls.addEventListener('change', this._threejs);
+      controls.minDistance = 500;
+      controls.maxDistance = 1500;
 
-    const box = new THREE.Mesh(
-      new THREE.BoxGeometry(2, 2, 2),
-      new THREE.MeshStandardMaterial({
-          color: 0xFFFFFF,
-      }));
-    box.position.set(0, 1, 0);
-    box.castShadow = true;
-    box.receiveShadow = true;
-    this._scene.add(box);
+        const loader = new THREE.CubeTextureLoader();
+        const texture = loader.load([
+            './resources/posx.jpg',
+            './resources/negx.jpg',
+            './resources/posy.jpg',
+            './resources/negy.jpg',
+            './resources/posz.jpg',
+            './resources/negz.jpg',
+        ]);
+        this._scene.background = texture;
 
-    for (let x = -8; x < 8; x++) {
-      for (let y = -8; y < 8; y++) {
+        var loader1 = new THREE.FontLoader();
+        console.log(this)
+
+        var fuckMeHard = this._scene;
+
+        loader1.load('fonts/helvetiker_regular.typeface.json', (font) => {
+
+          var geometry = new THREE.TextGeometry('Skylines One LLC is running! Mutter ficker!', {
+            font: font,
+            size: 40,
+            height: 2,
+            curveSegments: 12,
+            bevelEnabled: true,
+            bevelThickness: 2,
+            bevelSize: 0.5,
+            //bevelOffset: 0,
+            //bevelSegments: 5
+          });
+          geometry.center();
+          console.log(geometry)
+
+          var material = new THREE.MeshBasicMaterial( { color: 0x30C47E } );
+          const obj = new THREE.Mesh(geometry,material);
+          obj.position.set(0, 3.0, 0)
+
+          fuckMeHard.add(obj)
+        });
+
+        /*const plane = new THREE.Mesh(
+            new THREE.PlaneGeometry(100, 100, 10, 10),
+            new THREE.MeshStandardMaterial({
+                color: 0xFFFFFF,
+              }));
+        plane.castShadow = false;
+        plane.receiveShadow = true;
+        plane.rotation.x = -Math.PI / 2;
+        this._scene.add(plane);
+
         const box = new THREE.Mesh(
           new THREE.BoxGeometry(2, 2, 2),
           new THREE.MeshStandardMaterial({
-              color: 0x808080,
+              color: 0xFFFFFF,
           }));
-        box.position.set(Math.random() + x * 5, Math.random() * 4.0 + 2.0, Math.random() + y * 5);
+        box.position.set(0, 1, 0);
         box.castShadow = true;
         box.receiveShadow = true;
         this._scene.add(box);
-      }
+
+        for (let x = -8; x < 8; x++) {
+          for (let y = -8; y < 8; y++) {
+            const box = new THREE.Mesh(
+              new THREE.BoxGeometry(2, 2, 2),
+              new THREE.MeshStandardMaterial({
+                  color: 0x808080,
+              }));
+            box.position.set(Math.random() + x * 5, Math.random() * 4.0 + 2.0, Math.random() + y * 5);
+            box.castShadow = true;
+            box.receiveShadow = true;
+            this._scene.add(box);
+          }
+        }*/
+
+        // const box = new THREE.Mesh(
+        //   new THREE.SphereGeometry(2, 32, 32),
+        //   new THREE.MeshStandardMaterial({
+        //       color: 0xFFFFFF,
+        //       wireframe: true,
+        //       wireframeLinewidth: 4,
+        //   }));
+        // box.position.set(0, 0, 0);
+        // box.castShadow = true;
+        // box.receiveShadow = true;
+        // this._scene.add(box);
+
+        this._RAF();
     }
 
-    // const box = new THREE.Mesh(
-    //   new THREE.SphereGeometry(2, 32, 32),
-    //   new THREE.MeshStandardMaterial({
-    //       color: 0xFFFFFF,
-    //       wireframe: true,
-    //       wireframeLinewidth: 4,
-    //   }));
-    // box.position.set(0, 0, 0);
-    // box.castShadow = true;
-    // box.receiveShadow = true;
-    // this._scene.add(box);
+    _fuckUHardCoreJSIHateU(font) {
 
-    this._RAF();
-  }
+        var geometry = new THREE.TextGeometry('Hello three.js!', {
+            font: font,
+            size: 80,
+            height: 5,
+            curveSegments: 12,
+            bevelEnabled: true,
+            bevelThickness: 10,
+            bevelSize: 8,
+            bevelOffset: 0,
+            bevelSegments: 5
+        });
+        console.log(this)
+        this._scene.add(geometry)
+    }
 
-  _OnWindowResize() {
-    this._camera.aspect = window.innerWidth / window.innerHeight;
-    this._camera.updateProjectionMatrix();
-    this._threejs.setSize(window.innerWidth, window.innerHeight);
-  }
+    _OnWindowResize() {
+        this._camera.aspect = window.innerWidth / window.innerHeight;
+        this._camera.updateProjectionMatrix();
+        this._threejs.setSize(window.innerWidth, window.innerHeight);
+    }
 
-  _RAF() {
-    requestAnimationFrame(() => {
-      this._threejs.render(this._scene, this._camera);
-      this._RAF();
-    });
-  }
+    _RAF() {
+        requestAnimationFrame(() => {
+            this._threejs.render(this._scene, this._camera);
+            this._RAF();
+        });
+    }
 }
-
 
 let _APP = null;
 
 window.addEventListener('DOMContentLoaded', () => {
-  _APP = new BasicWorldDemo();
+    _APP = new BasicWorldDemo();
 });
+
